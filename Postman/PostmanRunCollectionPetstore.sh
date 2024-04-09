a99895ea023bd139e38287fff4ba9947908ba135
Vladimir Mochalov <Raket5420@gmail.com>
2024-04-09 19:00:31 +0300

    upload Postman

--- Postman/PostmanRunCollectionPetstore.sh
+++ Postman/PostmanRunCollectionPetstore.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-newman run Petstore_collection.postman_collection.json -e Petstore.postman_environment.json  > postman_run_collection_Petstore_log.txt
+newman run Postman/Petstore_collection.postman_collection.json -e Postman/Petstore.postman_environment.json  > Postman/postman_run_collection_Petstore_log.txt
